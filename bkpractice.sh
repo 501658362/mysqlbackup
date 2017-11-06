bb446c7080f0c2439cc5499d0d697a113d1567c0
chenyanjin <peng501658@gmail.com>
2017-11-06 11:06:37 +0800

    update sql

--- bkpractice.sh
+++ bkpractice.sh
@@ -2,4 +2,10 @@
 mysqldump -uroot -proot practice > /root/backup/mysql/practice_$(date +%Y%m%d_%H%M%S).sql
 #!/bin/bash
 #mysqldump -uroot -proot practice  | gzip > /root/backup/mysql/practice_$(date +%Y%m%d_%H%M%S).sql.gz
-
+#!/bin/bash
+echo ww
+#!/bin/bash
+cd /root/backup/mysql/
+git add . 
+git commit -m "update sql"
+git push -u origin master
