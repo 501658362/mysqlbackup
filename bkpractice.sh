c0ef84339fbc2d3158806d1ec4327ee5476b9eab
chenyanjin <peng501658@gmail.com>
2018-04-03 17:28:57 +0800

    update sql

--- bkpractice.sh
+++ bkpractice.sh
@@ -3,6 +3,7 @@ mysqldump -uroot -proot practice > /root/backup/mysql/practice_$(date +%Y%m%d_%H
 #mysqldump -uroot -proot practice  | gzip > /root/backup/mysql/practice_$(date +%Y%m%d_%H%M%S).sql.gz
 #!/bin/bash
 cd /root/backup/mysql/
+find /logs/ -mtime +60 -name "*.sql" -exec rm -rf {} \;
 git pull
 git add . 
 git commit -m "update sql"
