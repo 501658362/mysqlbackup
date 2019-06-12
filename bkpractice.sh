551cac9e7e8b4637cf117192c51e48853aa8a8e9
chenyanjin <peng501658@gmail.com>
2019-06-12 17:47:11 +0800

    update sql

--- bkpractice.sh
+++ bkpractice.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # xxxx
-mysqldump -uroot -proot practice > /root/backup/mysql/practice_$(date +%Y%m%d_%H%M%S).sql
+mysqldump -uroot -p501658 practice > /root/backup/mysql/practice_$(date +%Y%m%d_%H%M%S).sql
 #mysqldump -uroot -proot practice  | gzip > /root/backup/mysql/practice_$(date +%Y%m%d_%H%M%S).sql.gz
 #!/bin/bash
 cd /root/backup/mysql/
 find /root/backup/mysql/ -mtime +60 -name "*.sql" -exec rm -rf {} \;
 git pull
-git add . 
+git add -A
 git commit -m "update sql"
 git push -u origin master
